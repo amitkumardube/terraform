cca65f7f7a043aad22a2db359174a63050351ff7
Amit Dube <amitkumardube@gmail.com>
2021-01-17 16:26:51 +0000

    adjusted remote module parameters

--- remote_module.tf
+++ remote_module.tf
@@ -1,4 +1,8 @@
 module "gcloud" {
   source  = "terraform-google-modules/gcloud/google"
   version = "2.0.3"
+  create_cmd_entrypoint  = "gcloud"
+  create_cmd_body        = "compute instances list"
+  destroy_cmd_entrypoint = "gcloud"
+  destroy_cmd_body       = "version"  
 }
